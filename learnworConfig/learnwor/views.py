9c88145bd85d132c00ddb8cb3893157e6045f6f4
JinSeon Kim <rlawlstjs5573@duksung.ac.kr>
2024-02-16 21:18:42 +0900

    Feat: 크롤링 연결

--- learnworConfig/learnwor/views.py
+++ learnworConfig/learnwor/views.py
@@ -1,8 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render, resolve_url
+import sys
+sys.path.append('../web')
+from crawling import fetch_news_article
 
 # Create your views here.
 
 def index(request):
+    before_news = "" #원문 변수 선언
+
+    if request.method == 'POST':
+        print('post 성공')
+        before_news = fetch_news_article(request.POST['inputLink'])
+        return render(request, 'learnwor/home.html', {'before_news': before_news})
 
     return render(request, 'learnwor/home.html')
 
